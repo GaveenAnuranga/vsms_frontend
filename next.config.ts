b9ff9ff24fd4de8527ab0188de52d87d26673337
gaveen A Manamperi <gvn.020601@gmail.com>
2026-02-27 10:54:47 +0530

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -31,21 +31,15 @@ const nextConfig = {
   },
   env: {
     NEXT_APP_VERSION: 'v3.0.0',
-    NEXTAUTH_SECRET: 'LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx5mLg=',
-    NEXTAUTH_URL: 'http://localhost:3000/',
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || 'LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx5mLg=',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000/',
     NEXT_APP_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
-    NEXT_APP_API_URL: 'https://mock-data-api-nextjs.vercel.app',
-    NEXT_APP_JWT_SECRET: 'ikRgjkhi15HJiU78-OLKfjngiu',
-    env: {
-    NEXT_APP_VERSION: 'v3.0.0',
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXT_APP_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
-    NEXT_APP_API_URL: process.env.NEXT_APP_API_URL,
-    NEXT_APP_JWT_SECRET: process.env.NEXT_APP_JWT_SECRET,
+    NEXT_APP_API_URL: process.env.NEXT_APP_API_URL || 'https://mock-data-api-nextjs.vercel.app',
+    NEXT_APP_JWT_SECRET: process.env.NEXT_APP_JWT_SECRET || 'ikRgjkhi15HJiU78-OLKfjngiu',
     NEXT_APP_JWT_TIMEOUT: process.env.NEXT_APP_JWT_TIMEOUT,
     NEXTAUTH_SECRET_KEY: process.env.NEXTAUTH_SECRET_KEY
   }
-  }
+};
+
+export default nextConfig;
